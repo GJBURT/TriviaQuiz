cacb073b11d59a510cb3553af72cf9684eb11a78
Geoffrey <geoffreyjburt@gmail.com>
2025-03-17 13:25:34 -0400

    Initial commit with the homePage.jsx file setup.

---
+++ src/homePage.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const HomePage = () => {
+    // Setting constant variables for the Users Name, Category for questions, and the Difficulty of the questions.
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('');
+    const [difficulty, setDifficulty] = useState('');
+    // Submit Handling
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div>
+            <h1>Welcome to the Trivia Quiz App!</h1>
+            {/* Form Creation */}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Category:
+                    <select
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        required
+                    >
+                        {/* Categories will be populated using API */}
+                    </select>
+                </label>
+                <label>
+                    Difficulty:
+                    <select
+                        value={difficulty}
+                        onChange={(e) => setDifficulty(e.target.value)}
+                        required
+                    >
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+                </label>
+                <button type="submit">Begin Quiz</button>
+            </form>
+        </div>
+    );
+};
+
+export default HomePage;
