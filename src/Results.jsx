0f39c432a45b6694d909361db9612ac769f5c3db
Geoffrey <geoffreyjburt@gmail.com>
2025-03-24 15:49:51 -0400

    Updated the Results page to personalize the users answer to show the users inputted name for their answers. Final commit.

--- src/Results.jsx
+++ src/Results.jsx
@@ -12,7 +12,7 @@ const Results = ({ selectedAnswers, questions, name}) => {
             {questions.map((question, index)=>(
                 <div key={index}>
                     <p>{question.question}</p>
-                    <p>Selected Answer: <strong>{selectedAnswers[index] || "No answer was selected"}</strong></p>
+                    <p>{name}'s Answer: <strong>{selectedAnswers[index] || "No answer was selected"}</strong></p>
                     <p>Correct Answer: <strong>{question.correct_answer}</strong></p>
                 </div>
             ))} <a href="HomePage.jsx">Restart</a>
